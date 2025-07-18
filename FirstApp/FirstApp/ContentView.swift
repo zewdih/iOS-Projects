390f7ef23b430fb9ccee563baffc15432af38a75
Zewdi Herring <61659044+zewdih@users.noreply.github.com>
2025-07-17 20:49:23 -0700

    Sending over files

---
+++ FirstApp/FirstApp/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  FirstApp
+//
+//  Created by Zewdi's Mac on 7/5/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        //Z-Stack has things sit on top of each other via depth
+        
+        ZStack {
+            Color(.lightGray)
+                .ignoresSafeArea()
+            
+            //V-Stack aligns elements vertically
+            
+            VStack {
+                Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                //To add color to the background, we need a color element
+                Image("tea")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+                    .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                
+                Text("Cold Brew Tea")
+                    .font(.largeTitle)
+                    .fontWeight(.semibold)
+                    .foregroundColor(.white)
+
+                
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Zewdi's Mac on 7/5/25.
+//
+
+import SwiftUI
+//this is the first piece of code thats get ran for my app
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            //First screen of the app, which is invoked via ContentView file
+            ContentView()
+        }
+    }
+}
